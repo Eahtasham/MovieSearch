3dc75acfc552ce65769371de6445637bdb284573
Eahtasham <164941353+Eahtasham@users.noreply.github.com>
2024-10-26 01:43:49 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // src/config/api.js
-export const API_KEY = '10f681585bb705d92d9a461b68c621f2'; // Replace with your actual API key
+export const API_KEY = '10f681585bb705d92d9a461b68c621f2'; 
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 export const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
